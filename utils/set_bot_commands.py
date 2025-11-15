c1f75841b30162a02f789a43f943644be25901dc
davronovuz <davronovtatu@gmail.com>
2025-11-15 16:30:30 +0500

    dasasas sdada

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("getwifi", "WiFi  parolini olish"),
+
 
         ]
     )
